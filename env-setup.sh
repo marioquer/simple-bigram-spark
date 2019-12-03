99c108a0ba0d0ec63e662b96756cc3c7c89dafbb
Junjie Que <marioquer@hotmail.com>
2019-12-03 13:42:11 -0500

    add setup script

---
+++ env-setup.sh
@@ -0,0 +1,12 @@
+# install jdk
+sudo apt-get install default-jdk -y
+
+# install scala
+wget www.scala-lang.org/files/archive/scala-2.13.0.deb
+sudo dpkg -i scala*.deb
+
+# install sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+sudo apt-get install sbt -y
